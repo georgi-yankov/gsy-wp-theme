b8579f3704b09382cf02cef28de6129cd6ef6b9f
Georgi Yankov <georgi.st.yankov@gmail.com>
2014-07-26 21:59:14 +0300

    pagination changes

--- functions/pagination.php
+++ functions/pagination.php
@@ -1,15 +1,17 @@
 <?php
 
-/* ==================================================================================================
+/* =============================================================================
   PAGINATION
-  ================================================================================================== */
+  =========================================================================== */
 
 function pagination($pages = '', $range = 4) {
-    $showitems = ($range * 2) + 1;
+    //$showitems = ($range * 2) + 1;
+    $showitems = 1;
 
     global $paged;
-    if (empty($paged))
+    if (empty($paged)) {
         $paged = 1;
+    }
 
     if ($pages == '') {
         global $wp_query;
@@ -22,38 +24,26 @@ function pagination($pages = '', $range = 4) {
     if (1 != $pages) {
         echo "<div id='pagination'>";
         echo "<p>";
-        if ($paged > 2 && $paged > $range + 1 && $showitems < $pages) {
-            echo "<a href='" . get_pagenum_link(1) . "'>&laquo; Начало</a>";
-        } else {
-            echo "<span>&laquo; Начало</span>";
-        }
         if ($paged > 1 && $showitems < $pages) {
-            echo "<a href='" . get_pagenum_link($paged - 1) . "'>Предишна</a>";
-        } else {
-            echo "<span>Предишна</span>";
+            echo "<a class=\"pagination-previous\" href='" . get_pagenum_link($paged - 1) . "'>" . __('Previous') . "</a>";
         }
 
         echo "<span class='pagination-numbers'>";
         for ($i = 1; $i <= $pages; $i++) {
             if (1 != $pages && (!($i >= $paged + $range + 1 || $i <= $paged - $range - 1) || $pages <= $showitems )) {
-                echo ($paged == $i) ? "<span class='pagination-current'>" . $i . "</span>" : "<a href='" . get_pagenum_link($i) . "'>" . $i . "</a>";
+                if ($paged == $i) {
+                    echo "<span class='pagination-current'>" . $i . "</span>";
+                } else {
+                    echo "<a href='" . get_pagenum_link($i) . "'>" . $i . "</a>";
+                }
             }
         }
         echo "</span><!-- .pagination-numbers -->";
 
         if ($paged < $pages && $showitems < $pages) {
-            echo "<a href=\"" . get_pagenum_link($paged + 1) . "\">Следваща</a>";
-        } else {
-            echo "<span>Следваща</span>";
-        }
-
-        if ($paged < $pages - 1 && $paged + $range - 1 < $pages && $showitems < $pages) {
-            echo "<a href='" . get_pagenum_link($pages) . "'>Край &raquo;</a>";
-        } else {
-            echo "<span>Край &raquo;</span>";
+            echo "<a class=\"pagination-next\" href=\"" . get_pagenum_link($paged + 1) . "\">" . __('Next') . "</a>";
         }
         echo "</p>";
-
         echo "</div><!-- #pagination -->";
     }
 }
