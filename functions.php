d301776cad54373e4efb3652a3c106ccc1cb4631
Georgi Yankov <georgi.st.yankov@gmail.com>
2014-08-17 18:53:29 +0300

    corrected 'post-img' size

--- functions.php
+++ functions.php
@@ -36,7 +36,7 @@ if (function_exists('add_theme_support')) {
 
 if (function_exists('add_image_size')) {
     add_image_size('carousel-img', 450, 230, true);
-    add_image_size('post-img', 570, 250, true);
+    add_image_size('post-img', 570, 256, true);
     add_image_size('portfolio-item-img-small', 268, 144, true);
 }
 
